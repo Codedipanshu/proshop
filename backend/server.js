715da3960c760f3fa8f94fd16ecbe5898d861cb1
Codedipanshu <codedipanshu@gmail.com>
2023-09-22 15:30:49 +0530

    Database connect & model data

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
 const port = process.env.PORT || 5000;
+
+connectDB(); //Connect to MongoDB
 
 const app = express();
 
@@ -15,8 +18,8 @@ app.get("/api/products", (req, res) => {
 });
 
 app.get("/api/products/:id", (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
 });
 
 app.listen(port, () => {
