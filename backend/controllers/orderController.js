2a599517c856f6900c570994e1f8ed6d0ba9e677
Dipanshu Agrawal <codedipanshu@gmail.com>
2023-11-19 21:45:17 +0530

    Order page

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -17,7 +17,7 @@ const addOrderItems = asyncHandler(async (req, res) => {
 
   if (orderItems && orderItems.length === 0) {
     res.status(400);
-    throw new Error('No order items');
+    throw new Error("No order items");
   } else {
     const order = new Order({
       orderItems: orderItems.map((x) => ({
@@ -58,9 +58,9 @@ const getOrderById = asyncHandler(async (req, res) => {
   );
 
   if (order) {
-    req.status(200).json(order);
+    res.json(order);
   } else {
-    req.status(404);
+    res.status(404);
     throw new Error("Order not found");
   }
 });
